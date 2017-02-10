1c3f5db6801b16e52d4e452793a688985e85acc1
Bob Scott <bob@zyzo.com>
2017-02-10 06:31:47 +0000

    Created partials and article destroy method.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,7 +37,13 @@ class ArticlesController < ApplicationController
   
   def show
     @article = Article.find(params[:id])
-    
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = 'Article deleted successfully.'
+    redirect_to articles_path
   end
   
   private
